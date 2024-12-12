18ef0f6ad4dec140c9f15cca3923ee5452c27914
Jeff <pctech5510@gmail.com>
2024-12-12 05:08:54 -0500

     

--- main.py
+++
@@ -1,9 +0,0 @@
-#Display a background story for the character.
-
-
-#Create a menu for the user to see their current hacks, money, hardware
-
-
-#Create a shopping menu for the player to purchase items and upgrades for the player
-
-

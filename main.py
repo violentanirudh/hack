d3ea3b7692fb62bb0e93ca36bdcac81a5f2c1f24
Jeff <pctech5510@gmail.com>
2024-12-14 13:28:51 -0500

    cleared screen after each prompt

---
+++ main.py
@@ -0,0 +1,39 @@
+# Display a background story for the character.
+import os
+
+os.system("cls")
+
+print('''
+In the bustling sprawl of Neon Haven, a city of towering skyscrapers and neon-lit alleyways, 
+you grew up in the shadows of technology. Your father, a disgraced programmer framed for a cybercrime
+he didn’t commit, left behind only an outdated laptop and a thirst for justice in your veins. You look
+to avenge you father and family name.
+ ''')
+input()
+os.system("cls")
+
+# Create a menu for the user to see their current hacks, money, hardware
+print(f'''
+#####################
+#  Inventory        |
+# 1. Hacks          |
+# 2. Hardware       |
+# 3. Money          |
+# 4. Exit           |
+#####################
+''')
+input()
+os.system("cls")
+
+# Create a shopping menu for the player to purchase items and upgrades for the player
+print(
+    '''
+##################################
+#  Hack Menu Version 0.1         |
+# 1. Wifi Hacks                  |
+# 2. Network Hacks               |
+# 3. System Hacks                |
+# 4. Exit                        |
+##################################
+
+''')

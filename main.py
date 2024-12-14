59c83285d4f8dad7cf22bc4538025f750e4811b4
Jeff <pctech5510@gmail.com>
2024-12-14 13:19:08 -0500

    Added menu options, and player menu

---
+++ main.py
@@ -0,0 +1,9 @@
+#Display a background story for the character.
+
+
+#Create a menu for the user to see their current hacks, money, hardware
+
+
+#Create a shopping menu for the player to purchase items and upgrades for the player
+
+
